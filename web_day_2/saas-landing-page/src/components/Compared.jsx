c230413b2a5a4e99e573b41fec0295ba5c7993b3
Muhammad Ranju <mdranju23@gmail.com>
2024-08-19 16:45:54 +0600

    fear: added new Compared section

---
+++ web_day_2/saas-landing-page/src/components/Compared.jsx
@@ -0,0 +1,85 @@
+/* eslint-disable no-irregular-whitespace */
+function Compared() {
+  return (
+    <section className="dark:text-slate-800 bg-slate-100 lg:py-20 py-16">
+      <div className="container mx-auto  lg:px-0 px-3 text">
+        <h1 className="text-center text-5xl font-extrabold">
+          Compared to competitors
+        </h1>
+        <div className="w-full flex flex-col justify-center items-center mt-10">
+          <div className="flex  bg-white p-2 py-3 rounded-xl font-bold lg:flex-row lg:w-[1000px] justify-between items-center mt-10 gap-x-10 ">
+            <span className="flex-1 text-center ">Features</span>
+            <span className="flex-1 text-center  ">Competitor</span>
+            <span className="flex-1 ">
+              <img
+                src="../../images/logo.png"
+                className="w-32 text-center lg:ml-28"
+                alt=""
+              />
+            </span>
+          </div>
+          <div className="flex   p-2 py-3 rounded-xl font-bold lg:flex-row lg:w-[1000px] justify-between items-center mt-10 gap-x-20 ">
+            <span className="flex-1 text-center ">Features</span>
+            <span className="flex-1 text-center  ">2% Fee</span>
+            <span className="flex-1 ">
+              <span className="w-32 text-center lg:ml-28">1% Fee</span>
+            </span>
+          </div>
+          <div className="flex  bg-white p-2 py-5 rounded-xl font-bold lg:flex-row lg:w-[1000px] justify-between items-center mt-10 gap-x-16 ">
+            <span className="flex-1 text-center ">And another</span>
+            <span className="flex-1 text-center  ">Decent</span>
+            <span className="flex-1 ">
+              <span className="w-32 text-center lg:ml-28">Impeccable</span>
+            </span>
+          </div>
+          <div className="flex   p-2 py-3 rounded-xl font-bold lg:flex-row lg:w-[1000px] justify-between items-center mt-10 gap-x-16 ">
+            <span className="flex-1 text-center ">And another</span>
+            <span className="flex-1 text-center  ">-</span>
+            <span className="flex-1 ">
+              <img
+                src="../../images/right-icon.png"
+                className="w-5 text-center ml-28"
+                alt=""
+              />
+            </span>
+          </div>
+          <div className="flex  bg-white p-2 py-5 rounded-xl font-bold lg:flex-row lg:w-[1000px] justify-between items-center mt-10 gap-x-16   ">
+            <span className="flex-1 text-center ">Features</span>
+            <span className="flex-1 text-center  ">-</span>
+            <span className="flex-1 ">
+              <img
+                src="../../images/right-icon.png"
+                className="w-5 text-center ml-28"
+                alt=""
+              />
+            </span>
+          </div>
+          <div className="flex   p-2 py-3 rounded-xl font-bold lg:flex-row lg:w-[1000px] justify-between items-center mt-10 gap-x-16 ">
+            <span className="flex-1 text-center ">Another one</span>
+            <span className="flex-1 text-center  ">-</span>
+            <span className="flex-1 ">
+              <img
+                src="../../images/right-icon.png"
+                className="w-5 text-center ml-28"
+                alt=""
+              />
+            </span>
+          </div>
+          <div className="flex  bg-white p-2 py-5 rounded-xl font-bold lg:flex-row lg:w-[1000px] justify-between items-center mt-10 gap-x-16">
+            <span className="flex-1 text-center ">And another</span>
+            <span className="flex-1 text-center  ">-</span>
+            <span className="flex-1 ">
+              <img
+                src="../../images/right-icon.png"
+                className="w-5 text-center ml-28"
+                alt=""
+              />
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Compared;
